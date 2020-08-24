6dcba3fcfd590f29ad32a731ff8dfc42d0b2596c
Eloquent-rose <keerthanaravikumarnaidu@gmail.com>
2020-08-24 14:56:20 +0530

    Adding User Pages

---
+++ unwanted/Database/amart .sql
@@ -0,0 +1,318 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 13, 2020 at 08:52 PM
+-- Server version: 10.4.13-MariaDB
+-- PHP Version: 7.4.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `amart`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `address`
+--
+
+CREATE TABLE `address` (
+  `AD_ID` int(11) NOT NULL,
+  `AD_LOGIN_ID` varchar(50) DEFAULT NULL,
+  `ADDRESS` varchar(255) DEFAULT NULL,
+  `PINCODE` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `AD_ID` varchar(50) NOT NULL,
+  `AD_EMAIL_ID` varchar(100) DEFAULT NULL,
+  `AD_PASSWORD` varchar(100) DEFAULT NULL,
+  `AD_FIRSTNAME` char(100) DEFAULT NULL,
+  `AD_LASTNAME` char(100) DEFAULT NULL,
+  `AD_PHNO` bigint(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cart`
+--
+
+CREATE TABLE `cart` (
+  `CRT_LOGIN_ID` varchar(50) DEFAULT NULL,
+  `CRT_PRD_ID` int(11) DEFAULT NULL,
+  `CRT_QUANTITY` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `category`
+--
+
+CREATE TABLE `category` (
+  `CAT_ID` int(11) NOT NULL,
+  `CAT_NAME` char(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cookie`
+--
+
+CREATE TABLE `cookie` (
+  `CK_ID` bigint(11) NOT NULL,
+  `CK_LOGIN_ID` varchar(50) DEFAULT NULL,
+  `CK_VALUE` varchar(300) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `login`
+--
+
+CREATE TABLE `login` (
+  `LOGIN_ID` varchar(50) NOT NULL,
+  `EMAIL_ID` varchar(100) DEFAULT NULL,
+  `USER_PASSWORD` varchar(255) DEFAULT NULL,
+  `USER_FNAME` varchar(100) DEFAULT NULL,
+  `USER_PHNO` bigint(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+
+CREATE TABLE `orders` (
+  `ORD_ID` int(11) NOT NULL,
+  `ORD_LOGIN_ID` varchar(50) DEFAULT NULL,
+  `ORD_PRD_ID` int(11) DEFAULT NULL,
+  `ORD_QUANTITY` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `prd_image`
+--
+
+CREATE TABLE `prd_image` (
+  `IMG_ID` int(11) NOT NULL,
+  `IMG_PATH` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product`
+--
+
+CREATE TABLE `product` (
+  `PRD_CAT_ID` int(11) DEFAULT NULL,
+  `PRD_ID` int(11) NOT NULL,
+  `PRD_NAME` varchar(100) DEFAULT NULL,
+  `PRD_DETAILS` varchar(255) DEFAULT NULL,
+  `PRD_PRICE` float DEFAULT NULL,
+  `PRD_IMG_ID` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `total_amt`
+--
+
+CREATE TABLE `total_amt` (
+  `AMT_ID` int(11) NOT NULL,
+  `TA_ORDER_ID` int(11) DEFAULT NULL,
+  `TOTAL_AMT` float DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `address`
+--
+ALTER TABLE `address`
+  ADD PRIMARY KEY (`AD_ID`),
+  ADD KEY `AD_LOGIN_ID` (`AD_LOGIN_ID`);
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`AD_ID`),
+  ADD UNIQUE KEY `AD_EMAIL_ID` (`AD_EMAIL_ID`);
+
+--
+-- Indexes for table `cart`
+--
+ALTER TABLE `cart`
+  ADD KEY `CRT_LOGIN_ID` (`CRT_LOGIN_ID`),
+  ADD KEY `CRT_PRD_ID` (`CRT_PRD_ID`);
+
+--
+-- Indexes for table `category`
+--
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`CAT_ID`);
+
+--
+-- Indexes for table `cookie`
+--
+ALTER TABLE `cookie`
+  ADD PRIMARY KEY (`CK_ID`),
+  ADD KEY `COK_LOGIN_ID` (`CK_LOGIN_ID`);
+
+--
+-- Indexes for table `login`
+--
+ALTER TABLE `login`
+  ADD PRIMARY KEY (`LOGIN_ID`),
+  ADD UNIQUE KEY `EMAIL_ID` (`EMAIL_ID`);
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`ORD_ID`),
+  ADD KEY `ORD_LOGIN_ID` (`ORD_LOGIN_ID`),
+  ADD KEY `ORD_PRD_ID` (`ORD_PRD_ID`);
+
+--
+-- Indexes for table `prd_image`
+--
+ALTER TABLE `prd_image`
+  ADD PRIMARY KEY (`IMG_ID`);
+
+--
+-- Indexes for table `product`
+--
+ALTER TABLE `product`
+  ADD PRIMARY KEY (`PRD_ID`),
+  ADD KEY `PRD_CAT_ID` (`PRD_CAT_ID`),
+  ADD KEY `PRD_IMG_ID` (`PRD_IMG_ID`);
+
+--
+-- Indexes for table `total_amt`
+--
+ALTER TABLE `total_amt`
+  ADD PRIMARY KEY (`AMT_ID`),
+  ADD KEY `TA_ORDER_ID` (`TA_ORDER_ID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `address`
+--
+ALTER TABLE `address`
+  MODIFY `AD_ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `category`
+--
+ALTER TABLE `category`
+  MODIFY `CAT_ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `cookie`
+--
+ALTER TABLE `cookie`
+  MODIFY `CK_ID` bigint(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `ORD_ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `prd_image`
+--
+ALTER TABLE `prd_image`
+  MODIFY `IMG_ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `product`
+--
+ALTER TABLE `product`
+  MODIFY `PRD_ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `total_amt`
+--
+ALTER TABLE `total_amt`
+  MODIFY `AMT_ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `address`
+--
+ALTER TABLE `address`
+  ADD CONSTRAINT `address_ibfk_1` FOREIGN KEY (`AD_LOGIN_ID`) REFERENCES `login` (`LOGIN_ID`);
+
+--
+-- Constraints for table `cart`
+--
+ALTER TABLE `cart`
+  ADD CONSTRAINT `cart_ibfk_1` FOREIGN KEY (`CRT_LOGIN_ID`) REFERENCES `login` (`LOGIN_ID`),
+  ADD CONSTRAINT `cart_ibfk_2` FOREIGN KEY (`CRT_PRD_ID`) REFERENCES `product` (`PRD_ID`);
+
+--
+-- Constraints for table `cookie`
+--
+ALTER TABLE `cookie`
+  ADD CONSTRAINT `cookie_ibfk_1` FOREIGN KEY (`CK_LOGIN_ID`) REFERENCES `login` (`LOGIN_ID`);
+
+--
+-- Constraints for table `orders`
+--
+ALTER TABLE `orders`
+  ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`ORD_LOGIN_ID`) REFERENCES `login` (`LOGIN_ID`),
+  ADD CONSTRAINT `orders_ibfk_2` FOREIGN KEY (`ORD_PRD_ID`) REFERENCES `product` (`PRD_ID`);
+
+--
+-- Constraints for table `product`
+--
+ALTER TABLE `product`
+  ADD CONSTRAINT `product_ibfk_1` FOREIGN KEY (`PRD_CAT_ID`) REFERENCES `category` (`CAT_ID`),
+  ADD CONSTRAINT `product_ibfk_2` FOREIGN KEY (`PRD_IMG_ID`) REFERENCES `prd_image` (`IMG_ID`);
+
+--
+-- Constraints for table `total_amt`
+--
+ALTER TABLE `total_amt`
+  ADD CONSTRAINT `total_amt_ibfk_1` FOREIGN KEY (`TA_ORDER_ID`) REFERENCES `orders` (`ORD_ID`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ unwanted/mailsend.php
@@ -0,0 +1,222 @@
+<?php
+
+
+
+
+require '../_headers/PHPMailer/PHPMailerAutoload.php';
+
+$mail = new PHPMailer;
+
+//$mail->SMTPDebug = 3;                               // Enable verbose debug output
+
+$mail->SMTPDebug = 1;
+$mail->isSMTP();                                      // Set mailer to use SMTP
+$mail->Host = 'smtp.gmail.com';  //'smtp1.example.com;smtp2.example.com';  // Specify main and backup SMTP servers
+$mail->SMTPAuth = true;                               // Enable SMTP authentication
+$mail->Username = '';                 // SMTP username
+$mail->Password = '';                           // SMTP password
+$mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 587;                                    // TCP port to connect to
+
+$mail->setFrom('no-replay@gmail.com', 'Big Baazar');
+
+$mail->addAddress('keerthanaravikumarnaidu@gmail.com');     // Add a recipient
+
+
+
+$message = '<html><body>';
+$message .= '<h1>Hello, World!</h1>';
+$message .= '</body></html>';
+$message = '<html><body>';
+$message .= '<img src="//css-tricks.com/examples/WebsiteChangeRequestForm/images/wcrf-header.png" alt="Website Change Request" />';
+$message .= '<table rules="all" style="border-color: #666;" cellpadding="10">';
+$message .= "<tr style='background: #eee;'><td><strong>Name:</strong> </td><td>" . "<strong> Trying out the HTML and CSS in emails </strong>" . "</td></tr>";
+$message .= "<tr><td><strong>Email:</strong> </td><td>" . "<strong> Trying out the HTML and CSS in emails </strong>" . "</td></tr>";
+$message .= "<tr><td><strong>Type of Change:</strong> </td><td>" . "<strong> Trying out the HTML and CSS in emails </strong>" . "</td></tr>";
+$message .= "<tr><td><strong>Urgency:</strong> </td><td>" . "<strong> Trying out the HTML and CSS in emails </strong>" . "</td></tr>";
+$message .= "<tr><td><strong>URL To Change (main):</strong> </td><td>" . "<strong> Trying out the HTML and CSS in emails </strong>" . "</td></tr>";
+$message .= "<tr><td><strong>NEW Content:</strong> </td><td>" . "<strong> Trying out the HTML and CSS in emails </strong>" . "</td></tr>";
+$message .= "</table>";
+$message .= "</body></html>";
+
+
+
+
+// $mail->addAddress('ellen@example.com');               // Name is optional
+
+// $mail->addReplyTo('info@example.com', 'Information');
+// $mail->addCC('cc@example.com');
+// $mail->addBCC('bcc@example.com');
+
+// $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+// $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+$mail->isHTML(true);                                  // Set email format to HTML
+$message = "<strong> Gube Keerthana! </strong>";
+
+$mail->Subject = 'Notice from Big Baazar';
+$mail->Body    = $message;//'This is the HTML message body <b>in bold!</b>';
+$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+if(!$mail->send()) {
+    echo 'Message could not be sent.';
+    echo 'Mailer Error: ' . $mail->ErrorInfo;
+} else {
+    echo 'Message has been sent';
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// $to = 'keerthanaravikumarnaidu@example.com';
+
+// $subject = 'How/'s the Css';
+
+// $headers = "From: " . strip_tags($_POST['req-email']) . "\r\n";
+// $headers .= "Reply-To: ". strip_tags($_POST['req-email']) . "\r\n";
+// $headers .= "CC: susan@example.com\r\n";
+// $headers .= "MIME-Version: 1.0\r\n";
+// $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+
+
+
+// $to = "keerthanaravikumarnaidu@example.com";
+// $subject = "How's the Css";
+
+// // $message = "<b>This is HTML message.</b>";
+// // $message .= "<h1>This is headline.</h1>";
+
+// $header = "From:abc@somedomain.com \r\n";
+// $header .= "Cc:afgh@somedomain.com \r\n";
+// $header .= "MIME-Version: 1.0\r\n";
+// $header .= "Content-type: text/html\r\n";
+
+// $retval = mail ($to,$subject,$message,$header);
+
+// if( $retval == true ) {
+//    echo "Message sent successfully...";
+// }else {
+//    echo "Message could not be sent...";
+// }
+
+// error_reporting(-1);
+
+// $to_email = "keerthanaravikumarnaidu@gmail.com";
+// $subject = "Simple Email Test via PHP";
+// $body = "Hi,nn This is test email send by PHP Script";
+// $headers = "From: sender\'s email";
+
+// if(mail($to_email, $subject, $body, $headers)) 
+// {
+//     echo "Done";
+// //     echo "Email successfully sent to ";
+// } else {
+// //     echo "Email sending failed...";
+// echo "Not Done";
+// print_r(error_get_last());
+// ini_set('log_errors',TRUE);
+// ini_set('error_log','/path/to/logfile/error.log');
+
+// }
+
+
+// $message = '<html><body>';
+// $message .= '<h1>Hello, World!</h1>';
+// $message .= '</body></html>';
+// $message = '<html><body>';
+// $message .= '<img src="//css-tricks.com/examples/WebsiteChangeRequestForm/images/wcrf-header.png" alt="Website Change Request" />';
+// $message .= '<table rules="all" style="border-color: #666;" cellpadding="10">';
+// $message .= "<tr style='background: #eee;'><td><strong>Name:</strong> </td><td>" . "<strong> Trying out the HTML and CSS in emails </strong>" . "</td></tr>";
+// $message .= "<tr><td><strong>Email:</strong> </td><td>" . "<strong> Trying out the HTML and CSS in emails </strong>" . "</td></tr>";
+// $message .= "<tr><td><strong>Type of Change:</strong> </td><td>" . "<strong> Trying out the HTML and CSS in emails </strong>" . "</td></tr>";
+// $message .= "<tr><td><strong>Urgency:</strong> </td><td>" . "<strong> Trying out the HTML and CSS in emails </strong>" . "</td></tr>";
+// $message .= "<tr><td><strong>URL To Change (main):</strong> </td><td>" . "<strong> Trying out the HTML and CSS in emails </strong>" . "</td></tr>";
+// // $addURLS = $_POST['addURLS'];
+// // if (($addURLS) != '') {
+// //     $message .= "<tr><td><strong>URL To Change (additional):</strong> </td><td>" . "<strong> Trying out the HTML and CSS in emails </strong>" . "</td></tr>";
+// // }
+// // $curText = htmlentities($_POST['curText']);  
+// // if (($curText) != '') {
+// //     $message .= "<tr><td><strong>CURRENT Content:</strong> </td><td>" . $curText . "</td></tr>";
+// // }
+// $message .= "<tr><td><strong>NEW Content:</strong> </td><td>" . "<strong> Trying out the HTML and CSS in emails </strong>" . "</td></tr>";
+// $message .= "</table>";
+// $message .= "</body></html>";
+
+
+// $to = 'keerthanaravikumarnaidu@example.com';
+
+// $subject = 'How\'s the Css';
+
+// $headers = "From: " . strip_tags($_POST['req-email']) . "\r\n";
+// $headers .= "Reply-To: ". strip_tags($_POST['req-email']) . "\r\n";
+// $headers .= "CC: susan@example.com\r\n";
+// $headers .= "MIME-Version: 1.0\r\n";
+// $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+
+
+
+// $to = "keerthanaravikumarnaidu@example.com";
+// $subject = "How's the Css";
+
+// // $message = "<b>This is HTML message.</b>";
+// // $message .= "<h1>This is headline.</h1>";
+
+// $header = "From:abc@somedomain.com \r\n";
+// $header .= "Cc:afgh@somedomain.com \r\n";
+// $header .= "MIME-Version: 1.0\r\n";
+// $header .= "Content-type: text/html\r\n";
+
+// $retval = mail ($to,$subject,$message,$header);
+
+// if( $retval == true ) {
+//    echo "Message sent successfully...";
+// }else {
+//    echo "Message could not be sent...";
+// }
+
+// error_reporting(-1);
+
+// $to_email = "keerthanaravikumarnaidu@gmail.com";
+// $subject = "Simple Email Test via PHP";
+// $body = "Hi,nn This is test email send by PHP Script";
+// $headers = "From: sender\'s email";
+
+// if(mail($to_email, $subject, $body, $headers)) 
+// {
+//     echo "Done";
+// //     echo "Email successfully sent to ";
+// } else {
+// //     echo "Email sending failed...";
+// echo "Not Done";
+// print_r(error_get_last());
+// ini_set('log_errors',TRUE);
+// ini_set('error_log','/path/to/logfile/error.log');
+
+// }
